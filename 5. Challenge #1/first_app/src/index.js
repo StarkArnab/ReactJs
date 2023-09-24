f3cf157902a446710908df2ce2ef6cf1d3b3e54e
StarkArnab <rockarnab367@gmail.com>
2023-09-25 03:07:27 +0530

    Here I will create a react app from scratch and do some operations and will try to incorporate all the learnings that I have learnt so far

---
+++ 5. Challenge #1/first_app/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Netflix Picks</h1>
+    <p>Top 5 Picks are - </p>
+    <ol>
+      <li>Lucifer</li>
+      <li>You</li>
+      <li>One Punch Man</li>
+      <li>Sacred Games</li>
+      <li>One Piece</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
