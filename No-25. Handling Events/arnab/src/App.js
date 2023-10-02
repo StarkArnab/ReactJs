430c6fb0d86e61c181f1ddd8735ada45ee06ce41
StarkArnab <rockarnab367@gmail.com>
2023-10-02 12:28:29 +0530

    Here I have handled events and made some changes according to make it intersting for the user

---
+++ No-25. Handling Events/arnab/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [color, setColor] = useState("rgb(235, 160, 204)");
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("click me");
+  const BgChange = () => {
+    setCount(count + 1);
+    if (count === 0) {
+      setText("clicked");
+    } else if (count === 1) {
+      const bg = "purple";
+      setColor(bg);
+      setText("yay");
+    } else if (count === 2) {
+      const bg = "yellow";
+      setColor(bg);
+      setText("wassup");
+    } else if (count === 3) {
+      const bg = "brown";
+      setColor(bg);
+      setText("ayo");
+    } else {
+      setColor("rgb(235, 160, 204)");
+      setText("refresh");
+    }
+  };
+
+  return (
+    <>
+      <div style={{ backgroundColor: color }}>
+        <button onClick={BgChange}>{text}</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
